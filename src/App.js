c3948103bfec6f389fe13be0832e814c9f5faa1a
0xLil14n <0xLil14n@gmail.com>
2021-08-15 11:59:41 -0400

    adding cardlist grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {CardList} from './components/card-list/card-list.component';
 
-function App() {
-  return (
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 'one'
+        },
+        {
+          name: 'Dracula',
+          id: 'two'
+        },
+        {
+          name: 'Zombie',
+          id: 'three'
+        },
+      ]
+    }
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}))
+    
+  }
+
+  render() {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>sAWJF;OAIWEHGAO;EJF</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <CardList name="lili">
+      {
+        this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
+      }
+      </CardList>
     </div>
   );
+    }
 }
 
 export default App;
