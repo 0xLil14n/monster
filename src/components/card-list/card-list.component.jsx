86ec3cc20dfb2c719c7c344597c0cb9c74458478
0xLil14n <0xLil14n@gmail.com>
2021-08-23 12:56:25 -0500

    prettifying thingsss

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import {Card} from '../card/card.component';
-import styled from 'styled-components';
-export const CardList = props => {
-    return (<StyledCardList>
-        {props.monsters.map(monster => <Card key={monster.id} monster={monster}/>)}
-      </StyledCardList>)
-
-}
+import React from "react";
+import { Card } from "../card/card.component";
+import styled from "styled-components";
+export const CardList = (props) => {
+  return (
+    <StyledCardList>
+      {props.monsters.map((monster) => (
+        <Card key={monster.id} monster={monster} />
+      ))}
+    </StyledCardList>
+  );
+};
 const StyledCardList = styled.div`
-    width: 85vw;
-    margin: 0 auto;
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-    grid-gap: 20px;
+  width: 85vw;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: 20px;
 `;
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,27 +1,26 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 export const Card = (props) => {
-    return (
-        <StyledCard>
-            <h1>{props.monster.name}</h1>
-        </StyledCard>
-    )
+  return (
+    <StyledCard>
+      <h1>{props.monster.name}</h1>
+    </StyledCard>
+  );
 };
 const StyledCard = styled.div`
-    display: flex;
-    flex-direction: column;
-    background-color: #95dada;
-    border: 1px solid grey;
-    border-radius: 5px;
-    padding: 25px;
-    cursor: pointer;
-    -moz-osx-font-smoothing: grayscale;
-    backface-visibility: hidden;
-    transform: translate(0);
-    transition: transform 0.25s ease-out;
-    &:hover {
-        transform: scale(1.05);
-        
-    }
+  display: flex;
+  flex-direction: column;
+  background-color: #95dada;
+  border: 1px solid grey;
+  border-radius: 5px;
+  padding: 25px;
+  cursor: pointer;
+  -moz-osx-font-smoothing: grayscale;
+  backface-visibility: hidden;
+  transform: translate(0);
+  transition: transform 0.25s ease-out;
+  &:hover {
+    transform: scale(1.05);
+  }
 `;
