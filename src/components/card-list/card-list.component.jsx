8b624707e4dc7dd2d77234ba0d678883ec66b56d
0xLil14n <0xLil14n@gmail.com>
2021-08-23 12:46:27 -0500

    adding prettier + styled-components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import './card-list.styles.css';
 import {Card} from '../card/card.component';
+import styled from 'styled-components';
 export const CardList = props => {
-    return (<div className='card-list'>
+    return (<StyledCardList>
         {props.monsters.map(monster => <Card key={monster.id} monster={monster}/>)}
-      </div>)
+      </StyledCardList>)
 
 }
+const StyledCardList = styled.div`
+    width: 85vw;
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-gap: 20px;
+`;
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import './card.styles.css';
 import styled from 'styled-components';
 
 export const Card = (props) => {
     return (
-        <div className='card-container'>
+        <StyledCard>
             <h1>{props.monster.name}</h1>
-        </div>
+        </StyledCard>
     )
 };
+const StyledCard = styled.div`
+    display: flex;
+    flex-direction: column;
+    background-color: #95dada;
+    border: 1px solid grey;
+    border-radius: 5px;
+    padding: 25px;
+    cursor: pointer;
+    -moz-osx-font-smoothing: grayscale;
+    backface-visibility: hidden;
+    transform: translate(0);
+    transition: transform 0.25s ease-out;
+    &:hover {
+        transform: scale(1.05);
+        
+    }
+`;
