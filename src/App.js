819421d91b8257225fba86868cd68a563342f49a
0xLil14n <0xLil14n@gmail.com>
2021-08-23 12:24:08 -0500

    adding a card component

--- src/App.js
+++ src/App.js
@@ -33,11 +33,7 @@ class App extends Component {
     return (
     <div className="App">
       
-      <CardList name="lili">
-      {
-        this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
-      }
-      </CardList>
+      <CardList monsters={this.state.monsters}/>
     </div>
   );
     }
